fa9485ea0f291de1f52a6f8e1a82534b0cbc1062
Susan X. Huynh <xhuynh@mesosphere.com>
2018-08-07 09:16:07 -0700

    Added Kerberos config to YARN and Hive. With these changes I can run a basic Hive job with Kerberos, as principal HDFS (with HDFS keytab).

--- hive_pg/scripts/bootstrap.sh
+++ hive_pg/scripts/bootstrap.sh
@@ -10,11 +10,12 @@ printenv | cat >> /root/.bashrc
 sudo /etc/init.d/postgresql restart
 
 # create hdfs directories
-/usr/sbin/sshd && $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh &&\
- $HADOOP_PREFIX/sbin/start-dfs.sh &&\
- $HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /user/root &&\
+$HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /user/root &&\
  $HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /apps/hive/warehouse
 
+# altering the hive-site configuration
+sed s/HOSTNAME/$HOSTNAME/ /usr/local/hive/conf/hive-site.xml.template > /usr/local/hive/conf/hive-site.xml
+sed s/HOSTNAME/$HOSTNAME/ /opt/files/hive-site.xml.template > /opt/files/hive-site.xml
 
 # start hive metastore server
 $HIVE_HOME/bin/hive --service metastore &
@@ -29,6 +30,6 @@ if [[ $1 == "-bash" ]]; then
   /bin/bash
 fi
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000;echo $PATH; done
-fi
+#if [[ $1 == "-d" ]]; then
+#  while true; do sleep 1000;echo $PATH; done
+#fi
